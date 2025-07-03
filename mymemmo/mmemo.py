72df4e221a91e35c92f77e4197039ee46700218c
minimin05 <Gimmiegimmie0102@gmail.com>
2025-07-03 19:06:56 +0700

    first commit

---
+++ mymemmo/mmemo.py
@@ -0,0 +1,7 @@
+weight = input("Enter your weight in kilogram: ")
+weight = float(weight) 
+height = input("Enter your height in meter: ")
+height = float(height)  
+
+bmi = weight / (height * height)    
+print ('Your BMI is' , bmi)
